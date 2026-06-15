bb8b2c852cd142642c03b59f488d0315f27bf785
Sam Slight <samuelslight31@hotmail.com>
2026-06-15 18:02:56 -0400

    too much to say

--- js/game.js
+++ js/game.js
@@ -15,7 +15,7 @@ const questions = [
     },
     {
         character: "Shrek",
-        image: "images/shrek.png",
+        image: "[https://www.pngfind.com/pngs/m/30-304033_shrek-head-png-shrek-png-transparent-png.png]",
         part: "skin",
         correctColor: "#7FBF4D"
     },
@@ -33,10 +33,67 @@ const questions = [
     },
     {
         character: "Scooby-Doo",
-        image: "images/scooby.png",
+        image: "[https://assets.stickpng.com/images/59f87a353cec115efb3623a4.png]",
         part: "fur",
         correctColor: "#8B5A2B"
-    }
+    },
+
+    // NEW ADDITIONS
+
+    {
+        character: "Sonic the Hedgehog",
+        image: "https://assets.stickpng.com/thumbs/580b57fcd9996e24bc43c337.png]",
+        part: "body",
+        correctColor: "#0F52BA"
+    },
+    {
+        character: "Pikachu",
+        image: "[https://www.freeiconspng.com/thumbs/pikachu-transparent/pikachu-transparent-hd-1.png]",
+        part: "cheeks",
+        correctColor: "#FF1C1C"
+    },
+    {
+        character: "Homer Simpson",
+        image: "https://upload.wikimedia.org/wikipedia/en/0/02/Homer_Simpson_2006.png",
+        part: "pants",
+        correctColor: "#3B5BA9"
+    },
+    {
+    character: "Mario",
+    image: "https://www.stickpng.com/img/games/new-super-mario-bros/mario",
+    part: "eyes",
+    correctColor: "#2B6CB0"
+},
+{
+    character: "Garfield",
+    image: "https://pngimg.com/image/109379",
+    part: "fur",
+    correctColor: "#FF8C00"
+},
+{
+    character: "Link",
+    image: "https://www.pngfind.com/mpng/hmxTmo_zelda-link-transparent-png-png-download/",
+    part: "outfit",
+    correctColor: "#2E7D32"
+},
+{
+    character: "Stewie Griffin",
+    image: "https://www.etsy.com/ca/listing/1465138045/stewie-griffin-family-guy-svg-pdf-png",
+    part: "suspenders",
+    correctColor: "#D00000"
+},
+{
+    character: "Kirby",
+    image: "https://freepngimg.com/png/14849-kirby-free-download-png/download",
+    part: "skin",
+    correctColor: "#FF6FAE"
+},
+{
+    character: "Rick Sanchez",
+    image: "https://pngimg.com/image/101088",
+    part: "hair",
+    correctColor: "#A7D8FF"
+}
 ];
 
 const colorMap = {
@@ -49,6 +106,13 @@ const colorMap = {
     Purple: "purple",
     Orange: "orange"
 };
+
+function shuffleArray(array) {
+    for (let i = array.length - 1; i > 0; i--) {
+        const j = Math.floor(Math.random() * (i + 1));
+        [array[i], array[j]] = [array[j], array[i]];
+    }
+}
 
 let current = 0;
 let score = 0;
@@ -122,9 +186,18 @@ function checkAnswer() {
     document.getElementById("score").textContent =
         `Score: ${score}`;
 
+    // 👇 APPLY COLOR TO IMAGE PART
+    const overlay = document.getElementById("color-overlay");
+    overlay.style.backgroundColor = selected;
+
     current++;
 
-    setTimeout(loadQuestion, 1500);
+    setTimeout(() => {
+        // reset overlay for next question
+        document.getElementById("color-overlay").style.backgroundColor = "transparent";
+        loadQuestion();
+    }, 1500);
 }
 
+shuffleArray(questions);
 loadQuestion();
